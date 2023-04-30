c494b98d2c3f1c9d8b29b702a02fae5a1b155cf2
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-30 12:07:07 +0900

    f-string

--- main.py
+++ main.py
@@ -18,3 +18,15 @@ print(s.title())
 print(s.upper())
 print(s.lower())
 print(s.replace('Satoru', 'Oka'))
+
+# python 3.6 f-strings
+a = 'a'
+print(f'a is {a}')
+
+x, y, z = 1, 2, 3
+print(f'a is {x}, {y}, {z}')
+print(f'a is {z}, {y}, {z}')
+
+name = 'Satoru'
+family = 'Oka'
+print(f'My name is {name} {family}.')
