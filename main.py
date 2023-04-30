0679d919b580d397e2131a6e6a5b96d3539de211
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-30 11:57:52 +0900

    string method

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-# string index and slice
-word = 'python'
-print(word[0])
-print(word[1])
-print(word[5])
-print(word[-1])
-print(word[-6])
-print('##########')
-print(word[0:2])
-print(word[2:5])
-print(word[:2])
-print(word[2:])
-print('##########')
-print(len(word))
-word = 'j' + word[1:]
-print(word[:])
-n = len(word)
-print(n)
+# string method
+s = 'My name is Satoru. Hi Satoru'
+print(s)
+
+# Is the specified string at the beginning?
+is_start = s.startswith('My')
+print(is_start)
+is_start = s.startswith('name')
+print(is_start)
+print("##########")
+# start
+print(s.find('Satoru'))
+# end
+print(s.rfind('Satoru'))
+print(s.count('Satoru'))
+print(s.capitalize())
+print(s.title())
+print(s.upper())
+print(s.lower())
+print(s.replace('Satoru', 'Oka'))
