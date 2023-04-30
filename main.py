15a57cf5f966ee38a6af2992b6935fbdb8aa1ad6
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-30 10:50:47 +0900

    print separation, new line

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-# Variable declaration
-num = 1
-name = 'Mike'
-is_ok = True
-
-# type() - Confirmation of type
-print(num, type(num)) # int
-print(name, type(name)) # str
-print(is_ok, type(is_ok)) # bool
-
-# Ok to overwrite values
-num_1 = 1
-name_str = '1'
-
-num_1 = name_str
-print(num_1, type(num_1)) # print string
-
-# type conversion
-new_num = int(num_1)
-print(num_1, type(new_num))
+# separation, new line
+print('Hi', 'Mike', sep=',', end='\n')
+print('Hi', 'Mike', sep=',', end='\n')
+print('Hi', 'Mike', 'Hi', 'jen', sep=',', end='.\n')
