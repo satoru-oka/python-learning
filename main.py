14973fa8cbdd3455cc7ae22c4059b7d9330d497b
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-30 10:05:17 +0900

    variable declaration

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+# Variable declaration
+num = 1
+name = 'Mike'
+is_ok = True
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# type() - Confirmation of type
+print(num, type(num)) # int
+print(name, type(name)) # str
+print(is_ok, type(is_ok)) # bool
 
+# Ok to overwrite values
+num_1 = 1
+name_str = '1'
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+num_1 = name_str
+print(num_1, type(num_1)) # print string
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# type conversion
+new_num = int(num_1)
+print(num_1, type(new_num))
