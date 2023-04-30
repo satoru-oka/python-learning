1aff081497558770fbacb9e7237274e212012114
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-30 11:46:13 +0900

    string index and slice

--- main.py
+++ main.py
@@ -1,42 +1,18 @@
-# string
-print('hello')
-print("hello")
-print("I don't know")
-print('I don\'t know')
-# single quote
-print('say "I don\'t know"')
-# double-quote
-print("say \"I dont't know\"")
-
-print('hello.\nHow are you?')
-print('C:\name\name')
-print(r'C\name\name')
-
-# Whitespace removal at line breaks
-print("##########")
-print("""
-line1
-line2
-line3
-""")
-print("##########")
-print("""\
-line1
-line2
-line3\
-""")
-print("##########")
-
-# Output 'Hi.' 3 times
-print('Hi.' * 3 + 'Mike.')
-
-# parentheses
-s = ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-     'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
-
-print(s)
-
-# backslash
-y = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' \
-    'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
-print(y)
+# string index and slice
+word = 'python'
+print(word[0])
+print(word[1])
+print(word[5])
+print(word[-1])
+print(word[-6])
+print('##########')
+print(word[0:2])
+print(word[2:5])
+print(word[:2])
+print(word[2:])
+print('##########')
+print(len(word))
+word = 'j' + word[1:]
+print(word[:])
+n = len(word)
+print(n)
