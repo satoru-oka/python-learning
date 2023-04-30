7e197a1bd96b8249ebff6af272e3b2da588c3085
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-30 22:33:40 +0900

    dictionary copy

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
-# dictionary method
-is_dict = {'x': 10, 'y': 20}
-print(is_dict)
-# help(is_dict) # class dict documents
-print(is_dict.keys()) # dict_keys(['x', 'y'])
-print(is_dict.values()) # dict_values([10, 20])
+# dictionary copy
 
-is_dict_update = {'x': 1000, 'j': 500}
-print(is_dict) # {'x':10, 'y':20}
-print(is_dict_update) # {'x': 1000, 'j': 500}
-is_dict.update(is_dict_update)
-print(is_dict) # {'x': 1000, 'y': 20, 'j': 500}
-print(is_dict['x']) # 1000
-print(is_dict.get('x')) # 1000
-# print(is_dict['z']) # KeyError: 'z'
-print(is_dict.get('z')) # None
-print(is_dict.pop('x')) # 1000
-print(is_dict) # {'y': 20, 'j': 500}
-print(is_dict.clear()) # None
-print(is_dict) # {}
-is_dict = {'a': 100, 'b': 'b'}
-print('a' in is_dict) # True
-print('j' in is_dict) # False
+x = {'a': 1}
+y = x
+print(y) # {'a': 1}
+y['a'] = 1000
+print(x) # {'a': 1000}
+print(y) # {'a': 1000}
+print(id(x)) # 4297025344
+print(id(y)) # 4297025344
+
+x = {'a': 1}
+y = x.copy()
+y['a'] = 1000
+print(x)
+print(y)
+print(id(x)) # 4297025600
+print(id(y)) # 4297386496
