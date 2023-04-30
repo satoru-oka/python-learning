27bdf5695e3608eab82c3e8f22037a6a35f6b5b3
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-30 20:49:27 +0900

    list copy

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-# list method
-is_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 3, 10]
-print(is_list.index(3))
-print(is_list.index(9))
-print(is_list)
-print(is_list.index(3, 3))
-print(is_list.count(3))
-is_list.sort(reverse=True)
-print(is_list)
-is_list.sort()
-print(is_list)
-# split() - specification to whitespace
-is_name = 'My name is Satoru'
-to_split = is_name.split(' ')
-print(to_split)
-# join() - # join with
-x = ' ######### ' .join(to_split)
-print(x)
-print(help(list))
+# cross-valued
+i = [1, 2, 3, 4, 5]
+j = i
+j[0] = 100
+print('j =', j)
+print('i =', i)
+print(id(j))
+print(id(j))
+
+# call by reference
+x = [1, 2, 3, 4, 5]
+y = x.copy()
+# y = x[:]
+y[0] = 100
+print('y =', y)
+print('x =', x)
+print(id(y))
+print(id(x))
