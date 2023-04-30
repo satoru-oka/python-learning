23e72f7951fc42cce7236f0814b761a195335cda
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-30 11:09:02 +0900

    mathematical

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-# separation, new line
-print('Hi', 'Mike', sep=',', end='\n')
-print('Hi', 'Mike', sep=',', end='\n')
-print('Hi', 'Mike', 'Hi', 'jen', sep=',', end='.\n')
+import math
+
+# Executable in CLI
+print(2 + 2)
+print(5 - 2)
+print((5 - 2) * 6)
+print(5 - 2 * 6)
+print(6 * 5 - 2)
+print(5 / 2)
+print(5//2)
+print(5 % 2)
+print(0.6)
+print(.6)
+print(5 * 5)
+print(5 * 5 * 5 * 5 * 5)
+print(5 ** 5)
+
+# sqrt
+result = math.sqrt(25)
+print(result)
+
+# log2
+log = math.log2(10)
+print(log)
+
+# help - Documentation of mathematical functions
+print(help(math))
