7f0f18d88d7bcdef4563cb31e7a9be9ae697976f
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-04-30 21:56:10 +0900

    dictionary

--- main.py
+++ main.py
@@ -1,30 +1,15 @@
-# tuple unpacking
-num_tuple = (10, 20)
-print(num_tuple) # (10, 20)
-
-x, y = num_tuple
-print(x, y) # 10, 20
-
-min, max = 0, 100
-print(min, max) # 0 100
-is_tuple = 0, 100, 200
-print(type(is_tuple)) # tuple
-
-a, b, c, d, e, f = 'satoru', '1', '1', '1', 'e', 'f'
-# best practice
-a = 'satoru'
-b = '1'
-
-i = 10
-j = 20
-tmp = i # 10
-i = j # 20
-j = tmp # 10
-
-print(i, j)
-
-a = 100
-b = 200
-print(a, b) # 100, 200
-a, b = b, a # 200, 100
-print(a, b) # 200, 100
+# dictionary
+is_dict = {'x': 10, 'y': 20}
+print(is_dict, type(is_dict)) # {'x': 10, 'y': 20} <class 'dict'>
+print(is_dict['x']) # 10
+print(is_dict['y']) # 20
+is_dict['x'] = 'xxxxx'
+print(is_dict, type(is_dict)) # {'x': 'xxxxx', 'y': 20} <class 'dict'>
+is_dict['z'] = 'zzzzz'
+print(is_dict) # {'x': 'xxxxx', 'y': 20, 'z': 'zzzzz'}
+is_dict[1] = 10000
+print(is_dict) # {'x': 'xxxxx', 'y': 20, 'z': 'zzzzz', 1: 10000}
+is_dict = dict(a=10, b=20)
+print(is_dict) # {'a': 10, 'b': 20}
+is_dict = dict([('a', 10), ('b', 20)])
+print(is_dict) # {'a': 10, 'b': 20}
