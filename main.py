4ac79f17c87cf4a868f11d085f4e543405a8276e
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-01 14:58:53 +0900

    while else statement

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# while and continue
 count = 0
+
 while count < 5:
     print(count)
     count += 1
-
-print('##########')
-count = 0
-while True:
-    if count >= 5:
-        break
-    if count == 2:
-        count += 1
-        continue
-    print(count)
-    count += 1
+else:
+    print('done')
