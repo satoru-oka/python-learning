52a17cee731af5450b8351b4fff5999be0e921ec
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-01 12:02:19 +0900

    if a line is longer

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-is_set = {1, 2, 3, 4, 5}
-print(is_set, type(is_set)) #{1, 2, 3, 4, 5} <class 'set'>
+s = 'aaaaaaaaaaaaaaaa'\
+    + 'bbbbbbbbbbbbbbbbbbbbb'
+print(s) # aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbb
 
-is_set.add(6)
-print(is_set) # {1, 2, 3, 4, 5, 6}
+s = ('aaaaaaaaaaaaaaaa'
+    + 'bbbbbbbbbbbbbbbbbbbbb')
+print(s) # aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbb
 
-is_set.add(6)
-print(is_set) # {1, 2, 3, 4, 5, 6}
+x = 1 + 1 +  1 + 1 +  1 + 1 +  1 + 1 +  1 + 1 +  1 + 1 \
+    + 1 + 1 +  1 + 1 +  1 + 1 +  1 + 1 +  1 + 1 +  1 + 1
+print(x) # 24
 
-is_set.remove(2)
-print(is_set) # {1, 3, 4, 5, 6}
-# help(set)
-is_set.clear()
-print(is_set) # set()
+x = (1 + 1 +  1 + 1 +  1 + 1 +  1 + 1 +  1 + 1 +  1 + 1
+    + 1 + 1 +  1 + 1 +  1 + 1 +  1 + 1 +  1 + 1 +  1 + 1)
+print(x) # 24
