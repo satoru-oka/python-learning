beaef6a68e0583d1a118bd68f0a662b917d50753
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-01 15:15:04 +0900

    for, break, continue

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-while True:
-    word = input('Enter:')
-    if word == 'ok':
-        break
-    print('next')
+# for, break, continue statement
+some_list = [1, 2, 3, 4, 5]
+i = 0
+
+while i < len(some_list):
+    print(some_list[i]) # 1 2 3 4 5
+    i += 1
+
+print('##########')
+
+for i in some_list:
+    print(i)
+
+print('##########')
+
+for s in 'abcde':
+    print(s)
+
+print('##########')
+
+for word in ['My', 'name', 'is', 'satoru']:
+    if word == 'name':
+        continue
+    print(word)
