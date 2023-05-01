4251239ed13e8cacbd5d192df1bb7388474454d3
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-01 16:07:04 +0900

    function difinition

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-# Processing dictionaries with for statements.
-is_dict = {'x': 100, 'y': 200}
+# Function definition
+def say_something():
+    s = 'hi'
+    return s
 
-print(is_dict.items())
+result = say_something()
+print(result)
 
-for k, v in is_dict.items():
-    print(k, ':', v)
+def what_is_this(color):
+    if color == 'red':
+        return 'tomato'
+    elif color == 'green':
+        return 'green pepper'
+    else:
+        return "I don't know"
 
-help(is_dict)
+result = what_is_this('red')
+print(result)
+result = what_is_this('black')
+print(result)
