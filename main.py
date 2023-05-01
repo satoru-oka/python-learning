e9d3895ba2ffabe4a505a5b889b3c78dbe9a9258
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-01 11:30:53 +0900

    set

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-# dictionary copy
+# set
+is_set_a = {1, 2, 3, 4, 5, 6, 7, 7, 7}
+print(is_set_a, type(is_set_a)) # {1, 2, 3, 4, 5, 6, 7} <class 'set'>
+is_set_b = {1, 2, 3, 4, 5, 8, 11, 15}
 
-x = {'a': 1}
-y = x
-print(y) # {'a': 1}
-y['a'] = 1000
-print(x) # {'a': 1000}
-print(y) # {'a': 1000}
-print(id(x)) # 4297025344
-print(id(y)) # 4297025344
+is_set_result = is_set_a - is_set_b
+print(is_set_result) # {6, 7}
 
-x = {'a': 1}
-y = x.copy()
-y['a'] = 1000
-print(x)
-print(y)
-print(id(x)) # 4297025600
-print(id(y)) # 4297386496
+is_set_result = is_set_b - is_set_a
+print(is_set_result) # {8, 11, 15}
+
+is_set_result = is_set_a & is_set_b
+print(is_set_result) # {1, 2, 3, 4, 5}
+
+# is_set_result = is_set_a + is_set_b
+# print(is_set_result) # TypeError: unsupported operand type(s) for +: 'set' and 'set'
+
+is_set_result = is_set_a | is_set_b
+print(is_set_result) # {1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 15}
+
+is_set_result = is_set_a ^ is_set_b
+print(is_set_result) # {6, 7, 8, 11, 15}
