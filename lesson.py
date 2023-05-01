6f98533d5f15d3ddfb4ac6e9dbfd49af432d764d
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-01 22:27:46 +0900

    absolute, relative path

--- lesson.py
+++ lesson.py
@@ -1,5 +1,7 @@
 # import lesson_package.utils
-from lesson_package import utils
+from lesson_package.tools import utils
+from lesson_package.talk import human
+# from lesson_package import utils as u
 # from lesson_package import utils say_twice
 
 # r = lesson_package.utils.say_twice('hello')
@@ -7,3 +9,5 @@ r = utils.say_twice('hello')
 # r = say_twice('hello')
 
 print(r)
+print(human.sing())
+print(human.cry())
