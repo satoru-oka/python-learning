f589e777ad804f6dae0543cc7fdc99d32ab6bef6
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-01 11:45:21 +0900

    set method

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-# set
-is_set_a = {1, 2, 3, 4, 5, 6, 7, 7, 7}
-print(is_set_a, type(is_set_a)) # {1, 2, 3, 4, 5, 6, 7} <class 'set'>
-is_set_b = {1, 2, 3, 4, 5, 8, 11, 15}
+is_set = {1, 2, 3, 4, 5}
+print(is_set, type(is_set)) #{1, 2, 3, 4, 5} <class 'set'>
 
-is_set_result = is_set_a - is_set_b
-print(is_set_result) # {6, 7}
+is_set.add(6)
+print(is_set) # {1, 2, 3, 4, 5, 6}
 
-is_set_result = is_set_b - is_set_a
-print(is_set_result) # {8, 11, 15}
+is_set.add(6)
+print(is_set) # {1, 2, 3, 4, 5, 6}
 
-is_set_result = is_set_a & is_set_b
-print(is_set_result) # {1, 2, 3, 4, 5}
-
-# is_set_result = is_set_a + is_set_b
-# print(is_set_result) # TypeError: unsupported operand type(s) for +: 'set' and 'set'
-
-is_set_result = is_set_a | is_set_b
-print(is_set_result) # {1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 15}
-
-is_set_result = is_set_a ^ is_set_b
-print(is_set_result) # {6, 7, 8, 11, 15}
+is_set.remove(2)
+print(is_set) # {1, 3, 4, 5, 6}
+# help(set)
+is_set.clear()
+print(is_set) # set()
