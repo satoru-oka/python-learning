1e39e5bce54872102ce5c36ab8d4ed0211e48d3f
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-01 15:22:51 +0900

    for else

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
-# for, break, continue statement
-some_list = [1, 2, 3, 4, 5]
-i = 0
+# for else statement
+for fruit in ['apple', 'banana', 'orange']:
+    if fruit == 'banana':
+        print('stop eating')
+        break
+    print(fruit)
+else:
+    print('I ate all!')
 
-while i < len(some_list):
-    print(some_list[i]) # 1 2 3 4 5
-    i += 1
-
-print('##########')
-
-for i in some_list:
-    print(i)
-
-print('##########')
-
-for s in 'abcde':
-    print(s)
-
-print('##########')
-
-for word in ['My', 'name', 'is', 'satoru']:
-    if word == 'name':
-        continue
-    print(word)
+for call in ['a', 'b', 'c']:
+    print('call', call)
+else:
+    print('call comp')
