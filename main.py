566562e93f37145abe8891055fb493e8396797b6
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-01 21:49:48 +0900

    try except

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-# global function
-print(globals())
+# try except
+is_list = [1, 2, 3]
+i = 5
+
+try:
+    is_list[0]
+except IndexError as ex:
+    print("Don't worry: {}".format(ex))
+except NameError as ex:
+    print(ex)
+except Exception as ex:
+    print('other:{}'.format(ex))
+else:
+    print('done')
+finally:
+    print('clean up')
