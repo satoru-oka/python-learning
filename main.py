4912bdd6e8956d7f39ae138bc43c6cc4e6b79da2
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-01 21:14:47 +0900

    list inclusion notation

--- main.py
+++ main.py
@@ -1,34 +1,31 @@
-# generator
-def counter(num=10):
-    for _ in range(num):
-        yield 'run'
+is_tuple_1 = (1, 2, 3, 4, 5)
+is_tuple_2 = (1, 2, 3, 4, 5)
 
-def greeting():
-    yield 'Good morning'
-    # for example
-    # for i in range(10000000000000000):
-    #     print(i)
-    yield 'Good afternoon'
-    yield 'Good night'
+is_list = []
+for i in is_tuple_1:
+    is_list.append(i)
 
-g = greeting()
-c = counter()
-print(next(g))
-print('@@@@@')
+print(is_list) # [1, 2, 3, 4, 5]
 
-print(next(c))
-print(next(c))
-print(next(c))
-print(next(c))
-print(next(c))
+is_list = [i for i in is_tuple_1]
+print(is_list) # [1, 2, 3, 4, 5]
 
-print(next(g))
-print('@@@@@')
+is_list = []
+for i in is_tuple_2:
+    if i % 2 == 0:
+        is_list.append(i)
 
-print(next(c))
-print(next(c))
-print(next(c))
-print(next(c))
-print(next(c))
+print(is_list) # [2, 4]
 
-print(next(g))
+is_list = [i for i in is_tuple_2 if i % 2 == 0]
+print(is_list) # [2, 4]
+
+is_list = []
+for i in is_tuple_1:
+    for j in is_tuple_2:
+        is_list.append(i * j)
+
+print(is_list) # 1, 2, 3, 4, 5, 2, 4, 6, 8, 10, 3, 6, 9, 12, 15, 4, 8, 12, 16, 20, 5, 10, 15, 20, 25]
+
+is_list = [i * j for i in is_tuple_1 for j in is_tuple_2]
+print(is_list) # 1, 2, 3, 4, 5, 2, 4, 6, 8, 10, 3, 6, 9, 12, 15, 4, 8, 12, 16, 20, 5, 10, 15, 20, 25]
