0908ab5078e49e0f4bbd35de20f1fb6a10739410
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-01 21:20:49 +0900

    dictionary entitlement

--- main.py
+++ main.py
@@ -1,31 +1,11 @@
-is_tuple_1 = (1, 2, 3, 4, 5)
-is_tuple_2 = (1, 2, 3, 4, 5)
+# dictionary inclusion notation
+date = ['mon', 'tue', 'wed']
+drink = ['coffee', 'milk', 'water']
 
-is_list = []
-for i in is_tuple_1:
-    is_list.append(i)
+is_dict = {}
+for x, y in zip(date, drink):
+    is_dict[x] = y
+print(is_dict) # {'mon': 'coffee', 'tue': 'milk', 'wed': 'water'}
 
-print(is_list) # [1, 2, 3, 4, 5]
-
-is_list = [i for i in is_tuple_1]
-print(is_list) # [1, 2, 3, 4, 5]
-
-is_list = []
-for i in is_tuple_2:
-    if i % 2 == 0:
-        is_list.append(i)
-
-print(is_list) # [2, 4]
-
-is_list = [i for i in is_tuple_2 if i % 2 == 0]
-print(is_list) # [2, 4]
-
-is_list = []
-for i in is_tuple_1:
-    for j in is_tuple_2:
-        is_list.append(i * j)
-
-print(is_list) # 1, 2, 3, 4, 5, 2, 4, 6, 8, 10, 3, 6, 9, 12, 15, 4, 8, 12, 16, 20, 5, 10, 15, 20, 25]
-
-is_list = [i * j for i in is_tuple_1 for j in is_tuple_2]
-print(is_list) # 1, 2, 3, 4, 5, 2, 4, 6, 8, 10, 3, 6, 9, 12, 15, 4, 8, 12, 16, 20, 5, 10, 15, 20, 25]
+is_dict = {x: y for x, y in zip(date, drink)}
+print(is_dict) # {'mon': 'coffee', 'tue': 'milk', 'wed': 'water'}
