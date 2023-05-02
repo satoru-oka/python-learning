900b0b32e1639c9d2f31f2b488231a15d516809b
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-02 14:59:06 +0900

    third party

--- lesson.py
+++ lesson.py
@@ -1,24 +1,16 @@
-# Built-in Types
-is_string = "falkdfjaeioqrgkja,czm,vnajfhaskl;fqierysa:gjakdf" \
-            "jaslkfajfkljasjfwqeskjadfaskl;jfdfbkjtio"
-is_dict = {}
-for x in is_string:
-    if x not in is_dict:
-        is_dict[x] = 0
-    is_dict[x] += 1
-print(is_dict)
+import sys
 
-is_dict = {}
-for x in is_string:
-    is_dict.setdefault(x, 0)
-    is_dict[x] += 1
-print(is_dict)
+from termcolor import colored, cprint
 
-from collections import defaultdict
+text = colored("Hello, World!", "red", attrs=["reverse", "blink"])
+print(text)
+cprint("Hello, World!", "green", "on_red")
 
-is_dict = defaultdict(int)
-for x in is_string:
-    is_dict[x] += 1
-print(is_dict)
+print_red_on_cyan = lambda x: cprint(x, "red", "on_cyan")
+print_red_on_cyan("Hello, World!")
+print_red_on_cyan("Hello, Universe!")
 
-print(is_dict['f'])
+for i in range(10):
+    cprint(i, "magenta", end=" ")
+
+cprint("Attention!", "red", attrs=["bold"], file=sys.stderr)
