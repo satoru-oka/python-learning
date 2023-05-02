6e690e7484589d6a27e1932da6a8fef1ea075256
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-02 17:36:54 +0900

    abstract class

--- lesson.py
+++ lesson.py
@@ -1,31 +1,33 @@
 # duck typing
-class Person(object):
+import abc
+class Person(metaclass=abc.ABCMeta):
     def __init__(self, age=1):
         self.age = age
-    def drive(self):
-        if self.age >= 18:
-            print('ok')
-        else:
-            raise Exception('No drive')
 
+    @abc.abstractmethod
+    def drive(self):
+        pass
 class Baby(Person):
     def __init__(self, age=1):
         if age < 18:
             super().__init__(age)
         else:
             raise ValueError
+    def drive(self):
+        raise Exception('No drive')
 class Adult(Person):
     def __init__(self, age=18):
         if age >= 18:
             super().__init__(age)
         else:
             raise ValueError
+    def drive(self):
+        print('ok')
 
 baby = Baby()
+# baby.drive()
 adult = Adult()
-adult.age = 2023
-# adult.age = 17
-
+adult.drive()
 class Car(object):
     def __init__(self, model=None):
         self.model = model
@@ -34,8 +36,6 @@ class Car(object):
     def ride(self, person):
         person.drive()
 
-car = Car()
-car.ride(adult)
 class ToyotaCar(Car):
     def run(self):
         print('fast')
