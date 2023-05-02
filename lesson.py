643e39f87d94425147825acbd7d4373f658ee48c
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-02 15:51:48 +0900

    constructor and destructor

--- lesson.py
+++ lesson.py
@@ -1,6 +1,7 @@
-# class definition
+# class initialization and class variable(property)
 class Person(object):
     # holds the value of its own class.
+    # constructor
     def __init__(self, name):
         self.name = name
     def say_something(self):
@@ -8,6 +9,11 @@ class Person(object):
         self.run(10)
     def run(self, num):
         print('run ' * num)
+    #destructor
+    def __del__(self):
+        print('good bye')
 
 person = Person('satoru')
 person.say_something()
+# del person
+print("@@@@@@@@@@@@@@@")
