96a7b6881cd2dac185ebc71ba9178648dd0c8de1
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-02 23:38:44 +0900

    file read

--- lesson.py
+++ lesson.py
@@ -1,3 +1,21 @@
-# file with statement
-with open('test.txt', 'w') as is_file:
-    is_file.write('Test\n')
+# file read
+s = """\
+AAA
+BBB
+CCC
+DDD
+"""
+with open('test.txt', 'r') as is_file:
+    while True:
+        chunk = 2
+        line = is_file.read(chunk)
+        print(line)
+        if not line:
+            break
+
+with open('test.txt', 'r') as is_file:
+    while True:
+        line = is_file.readline()
+        print(line, end='')
+        if not line:
+            break
