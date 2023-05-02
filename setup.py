23bdf139dfb419d6a397acddbf1c042891b51ddc
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-02 12:17:07 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python-learning',
+    version='1.0.0',
+    packages=['lesson_package', 'lesson_package.talk', 'lesson_package.tools'],
+    url='https://forexample.com',
+    license='Free',
+    author='satoru',
+    author_email='',
+    description='Sample package'
+)
