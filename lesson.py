1e4da886aa60085b92930137392af225c1bd5393
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-02 16:59:31 +0900

    property setting

--- lesson.py
+++ lesson.py
@@ -10,10 +10,27 @@ class ToyotaCar(Car):
         print('fast')
 
 class TeslaCar(Car):
-    def __init__(self, model='Model S', enable_auto_run=False):
+    def __init__(self, model='Model S',
+                 enable_auto_run=False,
+                 passwd='123'):
         super().__init__(model)
-        self.enable_auto_run = enable_auto_run
+        # self.enable_auto_run = enable_auto_run # freely rewritable
+        self._enable_auto_run = enable_auto_run # explicit access
+        # self.__enable_auto_run = enable_auto_run # own class access
+        self.passwd = passwd
+
+    @property
+    def enabel_auto_run(self):
+        return self._enable_auto_run
+
+    @enabel_auto_run.setter
+    def enable_auto_run(self, is_enable):
+        if self.passwd == '456':
+            self._enable_auto_run = is_enable
+        else:
+            raise ValueError
     def run(self):
+        # print(self.__enable_auto_run) # own class access
         print('super fast') # override
     def auto_run(self):
         print('auto run')
@@ -25,8 +42,9 @@ toyota_car = ToyotaCar('Lexus')
 print(toyota_car.model) # Lexus
 toyota_car.run()
 print('tesla car ->>>')
-tesla_car = TeslaCar()
-print(tesla_car.model) # Model S
-print(tesla_car.enable_auto_run) # False
-tesla_car.run()
-tesla_car.auto_run()
+tesla_car = TeslaCar('model m', passwd='456')
+tesla_car.enable_auto_run = True
+print(tesla_car.model) # model m
+print(tesla_car._enable_auto_run) # True
+tesla_car.run() # super fast
+tesla_car.auto_run() # auto run
