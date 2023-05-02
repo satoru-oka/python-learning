f3647a2b09564dd0af1e837eab4ba53b7aa42ba9
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-02 22:08:54 +0900

    with statement

--- lesson.py
+++ lesson.py
@@ -1,5 +1,3 @@
-# file create
-is_file = open('test.txt', 'w')
-is_file.write('Test\n')
-# print('My', 'name', 'is', 'Mike', sep='#', end='!', file=is_file)
-is_file.close()
+# file with statement
+with open('test.txt', 'w') as is_file:
+    is_file.write('Test\n')
