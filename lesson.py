60784aeb32a7346329c10b1d17ec275de3536290
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-02 18:15:17 +0900

    class variable

--- lesson.py
+++ lesson.py
@@ -1,20 +1,33 @@
-# multiple inheritance
+# class variable
 class Person(object):
-    def talk(self):
-        print('talk')
 
-    def run(self):
-        print('person run')
+    kind = 'human'
+    def __init__(self, name):
+        self.name = name
 
-class Car(object):
-    def run(self):
-        print('car run')
+    def who_are_you(self):
+        print(self.name, self.kind)
 
-class PersonCarRobot(Car, Person):
-    def fly(self):
-        print('fly')
+a = Person('A')
+a.who_are_you() # A human
+b = Person('B')
+b.who_are_you() # B human
 
-person_car_robot = PersonCarRobot()
-person_car_robot.run() # Car class
-person_car_robot.talk()
-person_car_robot.fly()
+class T(object):
+    def __init__(self):
+        self.words = []
+
+    def add_word(self, word):
+        self.words.append(word)
+
+test1 = T()
+test1.add_word('add 1')
+test1.add_word('add 2')
+print(test1)
+print(test1.words) # ['add 1', 'add 2']
+
+test2 = T()
+test2.add_word('add 3')
+test2.add_word('add 4')
+print(test2)
+print(test2.words) # ['add 3', 'add 4']
