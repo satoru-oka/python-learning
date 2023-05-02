8a7ef38c0c77d7733311ec07b718bb2e7c3ef024
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-02 15:47:52 +0900

    class init, property

--- lesson.py
+++ lesson.py
@@ -1,7 +1,13 @@
 # class definition
 class Person(object):
+    # holds the value of its own class.
+    def __init__(self, name):
+        self.name = name
     def say_something(self):
-        print('hello')
+        print('I am {}. hello'.format(self.name))
+        self.run(10)
+    def run(self, num):
+        print('run ' * num)
 
-person = Person()
+person = Person('satoru')
 person.say_something()
