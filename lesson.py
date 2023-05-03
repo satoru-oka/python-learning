e8cdf1f3549d1ab9dd1b3cd8535b84b8d9f885c0
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-03 14:36:56 +0900

    pyyaml

--- lesson.py
+++ lesson.py
@@ -1,23 +1,20 @@
-# config logging
-import configparser
+# yml
+import yaml
 
-config = configparser.ConfigParser()
-config['DEFAULT'] = {
-    'debug': True
-}
-config['web_server'] = {
-    'host': '127.0.0.1',
-    'port': 80
-}
-config['db_server'] = {
-    'host': '127.0.0.1',
-    'port': 3306
-}
-with open('config.ini', 'w') as config_file:
-    config.write(config_file)
+with open('config.yml', 'w') as yaml_file:
+    yaml.dump({
+        'web_server': {
+            'host': '127.0.0.1',
+            'port': 80
+        },
+        'db_server': {
+            'host': '127.0.0.1',
+            'port': 3306
+        }
+    }, yaml_file)
 
-config.read('config.ini')
-print(config['web_server'])
-print(config['web_server']['host'])
-print(config['web_server']['port'])
-print(config['DEFAULT']['debug'])
+# print(help(yaml))
+with open('config.yml', 'r') as yaml_file:
+    data = yaml.safe_load(yaml_file)
+    # print(data, type(data))
+    print(data['web_server']['host'])
