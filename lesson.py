b9574496ad3cd71b2da7f859b761cbd3e9f9a4d6
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-03 11:51:16 +0900

    datetime

--- lesson.py
+++ lesson.py
@@ -1,13 +1,44 @@
-# subprocess
+# date time
+import datetime
+import time
 import os
-import subprocess
+import shutil
 
-# os.system('ls')
-# subprocess.run(['ls', '-al'])
-# subprocess.run('ls -al | grep test', shell=True, check=True)
+now = datetime.datetime.now()
+print(now)
+print(now.isoformat())
+print(now.strftime('%d/%m/%y-%H%M%S%f')) # f - micro
+print('@@@@@')
+today = datetime.date.today()
+print(today)
+print(today.isoformat())
+print(today.strftime('%d/%m/%y'))
+print('@@@@@')
+t = datetime.time(hour=1, minute=10, second=5, microsecond=100)
+print(t)
+print(t.isoformat())
+print(t.strftime('%H_%M_%S_%f'))
+print('@@@@@@')
+print(now)
+d = datetime.timedelta(weeks=1)
+d = datetime.timedelta(days=365)
+d = datetime.timedelta(hours=1)
+d = datetime.timedelta(minutes=1)
+d = datetime.timedelta(seconds=1)
+d = datetime.timedelta(microseconds=1)
+print(now - d)
+print('@@@@@')
+print('#####')
+time.sleep(2)
+print('#####')
+print(time.time())
 
-p1 = subprocess.Popen(['ls', '-al'], stdout=subprocess.PIPE)
-p2 = subprocess.Popen(['grep', 'test'], stdin=p1.stdout, stdout=subprocess.PIPE)
-p1.stdout.close()
-output = p2.communicate()[0]
-print(output)
+print('@@@@@')
+
+file_name = 'text.txt'
+
+if os.path.exists(file_name):
+    shutil.copy(file_name, "{}.{}".format(
+        file_name, now.strftime('%Y_%m_%d_%H_%M_%S')))
+with open(file_name, 'w') as f:
+    f.write('test')
