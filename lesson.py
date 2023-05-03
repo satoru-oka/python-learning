a5c2ac78dc53c2aa46dcf28e3114febd804b14f8
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-03 10:10:38 +0900

    csv

--- lesson.py
+++ lesson.py
@@ -1,7 +1,17 @@
-import string
+# csv
+import csv
 
-with open('design/email_template.txt') as f:
-    tempfile = string.Template(f.read())
+with open('test.csv', 'w') as csv_file:
+    fieldnames = ['Name', 'Count']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerow({'Name': 'A', 'Count': 1})
+    writer.writerow({'Name': 'B', 'Count': 2})
+    writer.writerow({'Name': 'B', 'Count': 3})
+    writer.writerow({'Name': 'B', 'Count': 4})
+    writer.writerow({'Name': 'B', 'Count': 5})
 
-contents = tempfile.substitute(name='Satoru', contents='How ar you?')
-print(contents)
+with open('test.csv', 'r') as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        print(row['Name'], row['Count'])
