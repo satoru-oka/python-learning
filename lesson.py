7cdd148cfcecb0162a9543b7ab9c74f46d0ee04d
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-03 22:00:20 +0900

    socketserver, http.server, webbrowser

---
+++ lesson.py
@@ -0,0 +1,7 @@
+# socketserver and http.server, webbrowser
+import http.server
+import socketserver
+
+with socketserver.TCPServer(('127.0.0.1', 8000),
+                            http.server.SimpleHTTPRequestHandler) as httpd:
+    httpd.serve_forever()
