41a3a21f78df652bb8dc8450cab3a14f8306e358
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-03 21:24:53 +0900

    requests

--- lesson.py
+++ lesson.py
@@ -1,29 +1,14 @@
-# urllib.request
-import urllib.request
-import json
+# request
+import requests
 
 payload = {'key1': 'value1', 'key2': 'value2'}
 
-# url = 'http://httpbin.org/get' + '?' + urllib.parse.urlencode(payload)
-# print(url)
-# with urllib.request.urlopen(url) as response_file:
-#     response = json.loads(response_file.read().decode('utf-8'))
-#     print(type(response))
-#     print(response)
+# response = requests.get('http://httpbin.org./get', params=payload)
+# response = requests.post('http://httpbin.org./post', params=payload)
+# response = requests.put('http://httpbin.org./put', params=payload)
+response = requests.delete('http://httpbin.org./delete', params=payload, timeout=1)
 
-payload = json.dumps(payload).encode('utf-8')
-# req = urllib.request.Request(
-#     'http://httpbin.org/post', data=payload, method='POST')
-# with urllib.request.urlopen(req) as response:
-#     print(json.loads(response.read().decode('utf-8')))
-
-req = urllib.request.Request(
-    'http://httpbin.org/put', data=payload, method='PUT')
-with urllib.request.urlopen(req) as response:
-    print(json.loads(response.read().decode('utf-8')))
-
-req = urllib.request.Request(
-    'http://httpbin.org/delete', data=payload, method='DELETE')
-with urllib.request.urlopen(req) as response:
-    print(json.loads(response.read().decode('utf-8')))
+print(response.status_code)
+print(response.text)
+print(response.json())
 
