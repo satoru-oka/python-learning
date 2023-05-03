20cc4763102e572cf1e41e34d3f064fa874539e1
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-03 15:16:57 +0900

    handler

--- loggtest.py
+++ loggtest.py
@@ -3,6 +3,9 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+handler = logging.FileHandler('loggtest.log')
+logger.addHandler(handler)
+
 def do_something():
     logger.info('from logtest')
     logger.debug('from logtest debug')
