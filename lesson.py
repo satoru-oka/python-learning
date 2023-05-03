4df335f21d5fd12c4895bf4d8918566100e1dfbf
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-03 14:48:03 +0900

    formatter

--- lesson.py
+++ lesson.py
@@ -1,14 +1,17 @@
 # logging
 import logging
 
-logging.basicConfig(filename='text.txt', level=logging.DEBUG)
+# logging.basicConfig(filename='text.txt', level=logging.DEBUG)
+formatter = '%(asctime)s:%(message)s'
+# formatter = '%(levelname)s:%(message)s'
+logging.basicConfig(level=logging.INFO, format=formatter)
 
-logging.critical('critical') # default
-logging.error('error') # default
-logging.warning('warning') # default
-logging.info('info')
-logging.debug('debug')
+# logging.critical('critical') # default
+# logging.error('error') # default
+# logging.warning('warning') # default
+# logging.info('info')
+# logging.debug('debug')
 
-logging.info('info {}'.format('test'))
-logging.info('info %s %s' % ('test', 'test2'))
+# logging.info('info {}'.format('test'))
+# logging.info('info %s %s' % ('test', 'test2'))
 logging.info('info %s %s', 'test', 'test2')
