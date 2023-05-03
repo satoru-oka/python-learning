7c53528a6c73610f3d5c9744281c36a4eff9921b
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-03 14:17:06 +0900

    configparser

--- lesson.py
+++ lesson.py
@@ -1,4 +1,23 @@
-# code style check
-# pip install pep8 -> pip install pycodestyle GitHub issue #466
-# pip install flake8
-# pip install pylint
+# config logging
+import configparser
+
+config = configparser.ConfigParser()
+config['DEFAULT'] = {
+    'debug': True
+}
+config['web_server'] = {
+    'host': '127.0.0.1',
+    'port': 80
+}
+config['db_server'] = {
+    'host': '127.0.0.1',
+    'port': 3306
+}
+with open('config.ini', 'w') as config_file:
+    config.write(config_file)
+
+config.read('config.ini')
+print(config['web_server'])
+print(config['web_server']['host'])
+print(config['web_server']['port'])
+print(config['DEFAULT']['debug'])
