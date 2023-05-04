f13646ac7481834a467a3a473fe0952ce8eb7d80
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-04 12:40:28 +0900

    beautifulsoup

--- lesson.py
+++ lesson.py
@@ -1,87 +1,14 @@
-# flask
-import sqlite3
+# beautifulsoup
+from bs4 import BeautifulSoup
 
-from flask import Flask
-from flask import g
-from flask import render_template
-from flask import request
-from flask import Response
+import requests
 
-app =  Flask(__name__)
+html = requests.get('https://www.python.org')
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect('test_sqlite.db')
-    return db
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+soup = BeautifulSoup(html.text, 'lxml')
 
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+titles = soup.find_all('title')
+print(titles[0].text)
 
-@app.route('/employee', methods=['POST', 'PUT', 'DELETE'])
-@app.route('/employee/<name>', methods=['GET'])
-def employee(name=None):
-    db = get_db()
-    curs = db.cursor()
-    curs.execute(
-        'CREATE TABLE IF NOT EXISTS persons('
-        'id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)'
-    )
-    db.commit()
-
-    name = request.values.get('name', name)
-    if request.method == 'GET':
-        curs.execute('SELECT * FROM persons WHERE name = "{}"'.format(name))
-        person = curs.fetchone()
-        if not person:
-            return  "No", 404
-        user_id, name = person
-        return '{}:{}'.format(user_id, name), 200
-
-    if request.method == 'POST':
-        curs.execute('INSERT INTO persons(name) values("{}")'.format(name))
-        db.commit()
-        return 'created {}'.format(name), 201
-
-    if request.method == 'PUT':
-        new_name = request.values['new_name']
-        curs.execute('UPDATE persons set name = "{}" WHERE name = "{}"'.format(
-            new_name, name
-        ))
-        db.commit()
-        return 'updated {}: {}'.format(name, new_name), 200
-
-    if request.method == 'DELETE':
-        curs.execute('DELETE from persons WHERE name = "{}"'.format(name))
-        db.commit()
-        return 'deleted {}'.format(name), 200
-
-    curs.close()
-
-@app.route('/')
-def hello_world():
-    return 'top'
-
-@app.route('/hello')
-@app.route('/hello/<username>')
-def hello_world2(username=None):
-    # return 'hello word! {}'.format(username)
-    return render_template('hello.html', username=username)
-
-@app.route('/post', methods=['POST', 'PUT', 'DELETE'])
-def show_post():
-    return str(request.values)
-
-def main():
-    app.debug = True
-    app.run()
-
-if __name__ == '__main__':
-    main()
+intro = soup.find_all('div', {'class': 'introduction'})
+print(intro[0].text)
