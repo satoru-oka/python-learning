b4ac4b0296905ea8fd3574cb98205ed1ed7334c6
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-05 19:11:37 +0900

    pytest exception

--- test_calculation.py
+++ test_calculation.py
@@ -1,3 +1,5 @@
+import pytest
+
 import caluculation
 
 # def test_add_num_and_double():
@@ -9,3 +11,8 @@ class TestCal(object):
     def test_add_num_add_double(self):
         cal = caluculation.Cal()
         assert cal.add_num_and_double(1, 1, ) == 4
+
+    def test_add_num_add_double_raise(self):
+        with pytest.raises(ValueError):
+            cal = caluculation.Cal()
+            cal.add_num_and_double('1', '1')
