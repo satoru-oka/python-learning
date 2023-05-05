ffccd43f01f76a3f7028598d2d0cc7a08b2d4fcf
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-05 21:00:02 +0900

    pytest setup, teardown

--- test_calculation.py
+++ test_calculation.py
@@ -8,11 +8,25 @@ import caluculation
     # assert cal.add_num_and_double(1, 1, ) == 4
 
 class TestCal(object):
+    @classmethod
+    def setup_class(cls):
+        print('start')
+        cls.cal = caluculation.Cal()
+
+    @classmethod
+    def teardown_class(cls):
+        print('end')
+        del cls.cal
+    def setup_method(self, method):
+        print('method={}'.format(method.__name__))
+        # self.cal = caluculation.Cal()
+
+    def teardown_method(self, method):
+        print('method={}'.format(method.__name__))
+        # del self.cal
     def test_add_num_add_double(self):
-        cal = caluculation.Cal()
-        assert cal.add_num_and_double(1, 1, ) == 4
+        assert self.cal.add_num_and_double(1, 1, ) == 4
 
     def test_add_num_add_double_raise(self):
         with pytest.raises(ValueError):
-            cal = caluculation.Cal()
-            cal.add_num_and_double('1', '1')
+            self.cal.add_num_and_double('1', '1')
