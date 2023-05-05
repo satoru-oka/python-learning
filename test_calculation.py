65d72a97986fd5378609559216c1700ca23298c6
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-05 19:07:20 +0900

    pytest

--- test_calculation.py
+++ test_calculation.py
@@ -1,26 +1,11 @@
-import unittest
-
 import caluculation
 
-release_name = 'lesson'
-class CalTest(unittest.TestCase):
-    def setUp(self):
-        print('setup')
-        self.cal = caluculation.Cal()
+# def test_add_num_and_double():
+    # cal = caluculation.Cal()
+    # assert cal.add_num_and_double(1, 1, ) != 4
+    # assert cal.add_num_and_double(1, 1, ) == 4
 
-    def tearDown(self):
-        print('clean up')
-        del self.cal
-
-    # @unittest.skip('skip!')
-    @unittest.skipIf(release_name=='lesson', 'skip')
-    def test_add_num_and_double(self):
-        self.assertEqual(self.cal.add_num_and_double(1, 1), 4)
-
-    def test_add_num_and_double_raise(self):
-        with self.assertRaises(ValueError):
-            self.cal.add_num_and_double('1', '1')
-
-# CLI
-if __name__ == '__main__':
-    unittest.main()
+class TestCal(object):
+    def test_add_num_add_double(self):
+        cal = caluculation.Cal()
+        assert cal.add_num_and_double(1, 1, ) == 4
