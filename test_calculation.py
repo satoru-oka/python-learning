ac43d49276c1247524d1ff6eddb69e7c42e100b6
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-05 21:19:35 +0900

    ifixture

--- test_calculation.py
+++ test_calculation.py
@@ -1,8 +1,5 @@
 import pytest
-
 import caluculation
-
-is_release = True
 
 class TestCal(object):
     @classmethod
@@ -15,10 +12,11 @@ class TestCal(object):
         print('end')
         del cls.cal
 
-    def test_add_num_add_double(self):
+    def test_add_num_add_double(self, request):
+        os_name = request.config.getoption('--os-name')
+        print(os_name)
+        if os_name == 'mac':
+            print('ls')
+        elif os_name == 'windows':
+            print('dir')
         assert self.cal.add_num_and_double(1, 1, ) == 4
-
-    @pytest.mark.skipif(is_release==True, reason='skip')
-    def test_add_num_add_double_raise(self):
-        with pytest.raises(ValueError):
-            self.cal.add_num_and_double('1', '1')
