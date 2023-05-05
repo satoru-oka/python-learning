7d39e0421d775a34da0a51799d2942032e163c3d
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-05 18:32:05 +0900

    unit test

---
+++ test_calculation.py
@@ -0,0 +1,12 @@
+import unittest
+
+import caluculation
+
+class CalTest(unittest.TestCase):
+    def test_add_num_and_double(self):
+        cal = caluculation.Cal()
+        self.assertEqual(cal.add_num_and_dobule(1, 1), 4)
+
+# CLI
+if __name__ == '__main__':
+    unittest.main()
