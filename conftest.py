f74f9ae6efef197e0cc4af36f10652e404357ec3
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-05 21:43:07 +0900

    original fixture

--- conftest.py
+++ conftest.py
@@ -1,2 +1,12 @@
+import os
+import pytest
+
+@pytest.fixture
+def csv_file(tmpdir):
+    with open(os.path.join(tmpdir, 'test.csv'), 'w+') as c:
+        print('before')
+        yield c
+        print('after')
+
 def pytest_addoption(parser):
     parser.addoption('--os-name', default='linux', help='os name')
--- test_calculation.py
+++ test_calculation.py
@@ -14,8 +14,8 @@ class TestCal(object):
         print('end')
         del cls.cal
 
-    def test_add_num_add_double(self, tmpdir):
-        print(tmpdir)
+    def test_add_num_add_double(self, csv_file):
+        print(csv_file)
         assert self.cal.add_num_and_double(1, 1) == 4
 
     def test_save(self, tmpdir):
