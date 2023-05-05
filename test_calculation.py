06e98453c837aaa4b0380af826663505d292db13
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-05 21:10:31 +0900

    pytest skip

--- test_calculation.py
+++ test_calculation.py
@@ -2,10 +2,7 @@ import pytest
 
 import caluculation
 
-# def test_add_num_and_double():
-    # cal = caluculation.Cal()
-    # assert cal.add_num_and_double(1, 1, ) != 4
-    # assert cal.add_num_and_double(1, 1, ) == 4
+is_release = True
 
 class TestCal(object):
     @classmethod
@@ -17,16 +14,11 @@ class TestCal(object):
     def teardown_class(cls):
         print('end')
         del cls.cal
-    def setup_method(self, method):
-        print('method={}'.format(method.__name__))
-        # self.cal = caluculation.Cal()
 
-    def teardown_method(self, method):
-        print('method={}'.format(method.__name__))
-        # del self.cal
     def test_add_num_add_double(self):
         assert self.cal.add_num_and_double(1, 1, ) == 4
 
+    @pytest.mark.skipif(is_release==True, reason='skip')
     def test_add_num_add_double_raise(self):
         with pytest.raises(ValueError):
             self.cal.add_num_and_double('1', '1')
