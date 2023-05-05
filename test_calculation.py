d8b54a87d4109da5c982c1e52f9858ac29c1919c
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-05 18:37:57 +0900

    unittest exception

--- test_calculation.py
+++ test_calculation.py
@@ -7,6 +7,12 @@ class CalTest(unittest.TestCase):
         cal = caluculation.Cal()
         self.assertEqual(cal.add_num_and_dobule(1, 1), 4)
 
+
+    def test_add_num_and_double_raise(self):
+        cal = caluculation.Cal()
+        with self.assertRaises(ValueError):
+            cal.add_num_and_dobule('1', '1')
+
 # CLI
 if __name__ == '__main__':
     unittest.main()
